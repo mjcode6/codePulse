7fdcc39ff5e5230bb0fe5616ab40ea568077c62c
sivasuthan manojan <mjcode6@gmail.com>
2023-10-08 13:57:06 +0200

    I worked on create category by using post method for inserting information into the database

---
+++ API/CodePulse.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,60 @@
+﻿using CodePulse.Api.Data;
+using CodePulse.Api.Models.Domain;
+using CodePulse.Api.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.Api.Controllers
+{
+
+    // https local host /api/categories
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public CategoriesController(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+
+        // 
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            // Map Dto to Domain models
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await applicationDbContext.Categories.AddAsync(category);
+
+            await applicationDbContext.SaveChangesAsync();
+
+            // Map Domain To DTO
+
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+
+
+
+
+
+            return Ok(category);
+
+        }
+
+
+    }
+}
---
+++ API/CodePulse.Api/Models/DTO/CreateCategoryRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CodePulse.Api.Models.DTO
+{
+    public class CreateCategoryRequestDto
+    {
+        public string Name { get; set; }
+
+        public string UrlHandle { get; set; }
+    }
+}
