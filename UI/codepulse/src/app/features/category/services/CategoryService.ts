e03195cb15ae86cb784ea98e2932a3ea61c7b4e5
sivasuthan manojan <mjcode6@gmail.com>
2023-10-15 14:53:55 +0200

    I work on angular ui for get all categories and i move towords creating environments fils

--- UI/codepulse/src/app/features/category/services/CategoryService.ts
+++ UI/codepulse/src/app/features/category/services/CategoryService.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Category } from '../models/category.model';
 
 
 @Injectable({
@@ -15,5 +16,9 @@ export class CategoryService {
 
   addCategory(model: AddCategoryRequest): Observable<void> {
     return this.http.post<void>('https://localhost:7281/api/Categories',model);
+  }
+
+  getAllCategories(): Observable<Category[]>{
+       return this.http.get<Category[]>('https://localhost:7281/api/Categories');
   }
 }
