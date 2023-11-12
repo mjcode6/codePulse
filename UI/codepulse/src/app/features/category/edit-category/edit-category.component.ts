a4a3f6c470160f4da8397ef3f6dd10805ca037c1
sivasuthan manojan <mjcode6@gmail.com>
2023-11-12 12:59:50 +0100

    I work on create services and methode display single category and i will move toward update category methode

--- UI/codepulse/src/app/features/category/edit-category/edit-category.component.ts
+++ UI/codepulse/src/app/features/category/edit-category/edit-category.component.ts
@@ -2,6 +2,8 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { __param } from 'tslib';
+import { CategoryService } from '../services/CategoryService';
+import { Category } from '../models/category.model';
 
 @Component({
   selector: 'app-edit-category',
@@ -13,10 +15,13 @@ export class EditCategoryComponent implements OnInit, OnDestroy{
 id: string | null = null;
 paramsSubscription: Subscription | undefined;
 
+category?: Category;
 
 
 
-  constructor(private route: ActivatedRoute){
+
+  constructor(private route: ActivatedRoute,
+    private categoryService: CategoryService){
 
   }
   
@@ -24,10 +29,25 @@ paramsSubscription: Subscription | undefined;
    this.paramsSubscription =  this.route.paramMap.subscribe({
       next: (params) => {
        this.id =  params.get('id');
+
+      if(this.id){
+        // get the data from the api for this category id
+        this.categoryService.getCategoryById(this.id)
+        .subscribe({
+          next: (response) => {
+               this.category = response;
+          }
+        });
+      }
+
+
       }
     });
   }
 
+  onFormSubmit(): void{
+       console.log(this.category);
+  }
 
   ngOnDestroy(): void {
     this.paramsSubscription?.unsubscribe();
